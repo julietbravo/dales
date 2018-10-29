a5ac2e92e2bea415ae5e97b714004ff7a3c9e852
julietbravo <bart@vanstratum.com>
2018-10-29 13:24:47 +0000

    Added option to nudge boundaries at specified time scale (instead of time step)

--- src/modnudgeboundary.f90
+++ src/modnudgeboundary.f90
@@ -30,7 +30,7 @@ save
     logical :: lnudge_boundary = .false.
     real, dimension(:), allocatable :: nudgefac_west,  nudgefac_east
     real, dimension(:), allocatable :: nudgefac_south, nudgefac_north
-    real :: nudge_offset, nudge_width
+    real :: nudge_offset=-1, nudge_width=-1, tau=-1
 
 contains
     subroutine initnudgeboundary
@@ -44,7 +44,7 @@ contains
         !
         ! Read namelist settings
         !
-        namelist /NAMNUDGEBOUNDARY/ lnudge_boundary, nudge_offset, nudge_width
+        namelist /NAMNUDGEBOUNDARY/ lnudge_boundary, nudge_offset, nudge_width, tau
 
         if (myid==0) then
             open(ifnamopt, file=fname_options, status='old', iostat=ierr)
@@ -55,9 +55,11 @@ contains
             write(6, NAMNUDGEBOUNDARY)
             close(ifnamopt)
         end if
+
         call MPI_BCAST(lnudge_boundary, 1, mpi_logical, 0, comm3d, mpierr)
         call MPI_BCAST(nudge_offset,    1, my_real,     0, comm3d, mpierr)
         call MPI_BCAST(nudge_width,     1, my_real,     0, comm3d, mpierr)
+        call MPI_BCAST(tau,             1, my_real,     0, comm3d, mpierr)
 
         if (lnudge_boundary) then
             !
@@ -68,43 +70,50 @@ contains
 
             do i=2,i1
                 x = myidx * (xsize / nprocx) + (i-1.5)*dx
-                nudgefac_west(i) =  exp(-((x-       nudge_offset )**2. / (2*nudge_width**2.)))
-                nudgefac_east(i) =  exp(-((x-(xsize-nudge_offset))**2. / (2*nudge_width**2.)))
+                nudgefac_west(i) = exp(-0.5*((x-       nudge_offset )/nudge_width)**2)
+                nudgefac_east(i) = exp(-0.5*((x-(xsize-nudge_offset))/nudge_width)**2)
             end do
 
             do j=2,j1
                 y = myidy * (ysize / nprocy) + (j-1.5)*dy
-                nudgefac_south(i) =  exp(-((y-       nudge_offset )**2. / (2*nudge_width**2.)))
-                nudgefac_north(i) =  exp(-((y-(ysize-nudge_offset))**2. / (2*nudge_width**2.)))
+                nudgefac_south(j) = exp(-0.5*((y-       nudge_offset )/nudge_width)**2)
+                nudgefac_north(j) = exp(-0.5*((y-(ysize-nudge_offset))/nudge_width)**2)
             end do
         end if ! lnudge_boundary
     end subroutine initnudgeboundary
 
     subroutine nudgeboundary
-        use modglobal, only : i1, j1, kmax, rdt, cu, cv
+        use modglobal, only : i1, j1, kmax, rdt, cu, cv, eps1
         use modfields, only : u0, up, v0, vp, w0, wp, thl0, thlp, qt0, qtp, uprof, vprof, thlprof, qtprof
         implicit none
 
         integer :: i,j,k
+        real :: tau_i
 
         if (lnudge_boundary) then
+            if (tau <= eps1) then
+                tau_i = 1. / rdt  ! Nudge on time scale equal to current time step
+            else
+                tau_i = 1. / tau  ! Nudge on specified time scale
+            end if
+
             do k=1,kmax
                 do j=2,j1
                     do i=2,i1
-                        up(i,j,k)   = up(i,j,k)   + nudgefac_west(i) * (uprof(k)   - (u0(i,j,k)+cu)) / rdt + &
-                                                & + nudgefac_east(i) * (uprof(k)   - (u0(i,j,k)+cu)) / rdt
+                        up(i,j,k)   = up(i,j,k)   + nudgefac_west(i) * tau_i * (uprof(k)   - (u0(i,j,k)+cu)) + &
+                                                & + nudgefac_east(i) * tau_i * (uprof(k)   - (u0(i,j,k)+cu))
 
-                        vp(i,j,k)   = vp(i,j,k)   + nudgefac_west(i) * (vprof(k)   - (v0(i,j,k)+cv)) / rdt + &
-                                                & + nudgefac_east(i) * (vprof(k)   - (v0(i,j,k)+cv)) / rdt
+                        vp(i,j,k)   = vp(i,j,k)   + nudgefac_west(i) * tau_i * (vprof(k)   - (v0(i,j,k)+cv)) + &
+                                                & + nudgefac_east(i) * tau_i * (vprof(k)   - (v0(i,j,k)+cv))
 
-                        wp(i,j,k)   = wp(i,j,k)   + nudgefac_west(i) * (0.         - w0(i,j,k)) / rdt + &
-                                                & + nudgefac_east(i) * (0.         - w0(i,j,k)) / rdt
+                        wp(i,j,k)   = wp(i,j,k)   + nudgefac_west(i) * tau_i * (0.         - w0(i,j,k)) + &
+                                                & + nudgefac_east(i) * tau_i * (0.         - w0(i,j,k))
 
-                        thlp(i,j,k) = thlp(i,j,k) + nudgefac_west(i) * (thlprof(k) - thl0(i,j,k)) / rdt + &
-                                                & + nudgefac_east(i) * (thlprof(k) - thl0(i,j,k)) / rdt
+                        thlp(i,j,k) = thlp(i,j,k) + nudgefac_west(i) * tau_i * (thlprof(k) - thl0(i,j,k)) + &
+                                                & + nudgefac_east(i) * tau_i * (thlprof(k) - thl0(i,j,k))
 
-                        qtp(i,j,k)  = qtp(i,j,k)  + nudgefac_west(i) * (qtprof(k)  - qt0(i,j,k) ) / rdt + &
-                                                & + nudgefac_east(i) * (qtprof(k)  - qt0(i,j,k) ) / rdt
+                        qtp(i,j,k)  = qtp(i,j,k)  + nudgefac_west(i) * tau_i * (qtprof(k)  - qt0(i,j,k) ) + &
+                                                & + nudgefac_east(i) * tau_i * (qtprof(k)  - qt0(i,j,k) )
                     end do
                 end do
             end do
