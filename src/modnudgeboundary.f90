acccbfb9dab081d28f3a8c77039801b8a5ef4094
julietbravo <bart@vanstratum.com>
2018-10-30 12:39:22 +0100

    Added option to nudge boundary towards domain mean profiles

--- src/modnudgeboundary.f90
+++ src/modnudgeboundary.f90
@@ -28,14 +28,16 @@ implicit none
 public  :: initnudgeboundary, nudgeboundary, exitnudgeboundary
 save
     logical :: lnudge_boundary = .false.
+    integer :: nudge_mode = 1  ! 1=initial profile, 2=mean profile
     real, dimension(:), allocatable :: nudgefac_west,  nudgefac_east
     real, dimension(:), allocatable :: nudgefac_south, nudgefac_north
+    real, dimension(:), allocatable :: unudge, vnudge, thlnudge, qtnudge
     real :: nudge_offset=-1, nudge_width=-1, tau=-1
 
 contains
     subroutine initnudgeboundary
-        use modmpi,    only : myid, mpierr, comm3d, mpi_logical, my_real, myidx, myidy, nprocx, nprocy
-        use modglobal, only : ifnamopt, fname_options, i1, j1, dx, dy, xsize, ysize
+        use modmpi,    only : myid, mpierr, comm3d, mpi_logical, mpi_int, my_real, myidx, myidy, nprocx, nprocy
+        use modglobal, only : ifnamopt, fname_options, i1, j1, k1, dx, dy, xsize, ysize
         implicit none
 
         integer :: ierr, i, j
@@ -44,7 +46,7 @@ contains
         !
         ! Read namelist settings
         !
-        namelist /NAMNUDGEBOUNDARY/ lnudge_boundary, nudge_offset, nudge_width, tau
+        namelist /NAMNUDGEBOUNDARY/ lnudge_boundary, nudge_offset, nudge_width, tau, nudge_mode
 
         if (myid==0) then
             open(ifnamopt, file=fname_options, status='old', iostat=ierr)
@@ -57,6 +59,7 @@ contains
         end if
 
         call MPI_BCAST(lnudge_boundary, 1, mpi_logical, 0, comm3d, mpierr)
+        call MPI_BCAST(nudge_mode,      1, mpi_int,     0, comm3d, mpierr)
         call MPI_BCAST(nudge_offset,    1, my_real,     0, comm3d, mpierr)
         call MPI_BCAST(nudge_width,     1, my_real,     0, comm3d, mpierr)
         call MPI_BCAST(tau,             1, my_real,     0, comm3d, mpierr)
@@ -67,6 +70,7 @@ contains
             !
             allocate( nudgefac_west(2:i1),  nudgefac_east(2:i1) )
             allocate( nudgefac_south(2:j1), nudgefac_north(2:j1) )
+            allocate( unudge(k1), vnudge(k1), thlnudge(k1), qtnudge(k1) )
 
             do i=2,i1
                 x = myidx * (xsize / nprocx) + (i-1.5)*dx
@@ -84,7 +88,9 @@ contains
 
     subroutine nudgeboundary
         use modglobal, only : i1, j1, kmax, rdt, cu, cv, eps1
-        use modfields, only : u0, up, v0, vp, w0, wp, thl0, thlp, qt0, qtp, uprof, vprof, thlprof, qtprof
+        use modfields, only : u0, up, v0, vp, w0, wp, thl0, thlp, qt0, qtp, &
+                            & uprof, vprof, thlprof, qtprof, &
+                            & u0av,  v0av,  thl0av,  qt0av
         implicit none
 
         integer :: i,j,k
@@ -97,23 +103,37 @@ contains
                 tau_i = 1. / tau  ! Nudge on specified time scale
             end if
 
+            if (nudge_mode == 1) then
+                unudge   = uprof
+                vnudge   = vprof
+                thlnudge = thlprof
+                qtnudge  = qtprof
+            else if (nudge_mode == 2) then
+                unudge   = u0av
+                vnudge   = v0av
+                thlnudge = thl0av
+                qtnudge  = qt0av
+            else
+                stop "unsupported nudge_mode"
+            end if
+
             do k=1,kmax
                 do j=2,j1
                     do i=2,i1
-                        up(i,j,k)   = up(i,j,k)   + nudgefac_west(i) * tau_i * (uprof(k)   - (u0(i,j,k)+cu)) + &
-                                                & + nudgefac_east(i) * tau_i * (uprof(k)   - (u0(i,j,k)+cu))
+                        up(i,j,k)   = up(i,j,k)   + nudgefac_west(i) * tau_i * (unudge(k)   - (u0(i,j,k)+cu)) + &
+                                                & + nudgefac_east(i) * tau_i * (unudge(k)   - (u0(i,j,k)+cu))
 
-                        vp(i,j,k)   = vp(i,j,k)   + nudgefac_west(i) * tau_i * (vprof(k)   - (v0(i,j,k)+cv)) + &
-                                                & + nudgefac_east(i) * tau_i * (vprof(k)   - (v0(i,j,k)+cv))
+                        vp(i,j,k)   = vp(i,j,k)   + nudgefac_west(i) * tau_i * (vnudge(k)   - (v0(i,j,k)+cv)) + &
+                                                & + nudgefac_east(i) * tau_i * (vnudge(k)   - (v0(i,j,k)+cv))
 
-                        wp(i,j,k)   = wp(i,j,k)   + nudgefac_west(i) * tau_i * (0.         - w0(i,j,k)) + &
-                                                & + nudgefac_east(i) * tau_i * (0.         - w0(i,j,k))
+                        wp(i,j,k)   = wp(i,j,k)   + nudgefac_west(i) * tau_i * (0.          - w0(i,j,k)) + &
+                                                & + nudgefac_east(i) * tau_i * (0.          - w0(i,j,k))
 
-                        thlp(i,j,k) = thlp(i,j,k) + nudgefac_west(i) * tau_i * (thlprof(k) - thl0(i,j,k)) + &
-                                                & + nudgefac_east(i) * tau_i * (thlprof(k) - thl0(i,j,k))
+                        thlp(i,j,k) = thlp(i,j,k) + nudgefac_west(i) * tau_i * (thlnudge(k) - thl0(i,j,k)) + &
+                                                & + nudgefac_east(i) * tau_i * (thlnudge(k) - thl0(i,j,k))
 
-                        qtp(i,j,k)  = qtp(i,j,k)  + nudgefac_west(i) * tau_i * (qtprof(k)  - qt0(i,j,k) ) + &
-                                                & + nudgefac_east(i) * tau_i * (qtprof(k)  - qt0(i,j,k) )
+                        qtp(i,j,k)  = qtp(i,j,k)  + nudgefac_west(i) * tau_i * (qtnudge(k)  - qt0(i,j,k) ) + &
+                                                & + nudgefac_east(i) * tau_i * (qtnudge(k)  - qt0(i,j,k) )
                     end do
                 end do
             end do
@@ -125,6 +145,7 @@ contains
         implicit none
         if (lnudge_boundary) then
             deallocate( nudgefac_west, nudgefac_east, nudgefac_south, nudgefac_north )
+            deallocate( unudge, vnudge, thlnudge, qtnudge)
         end if
     end subroutine exitnudgeboundary
 
