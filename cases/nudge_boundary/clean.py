05e522272fe1793921c81e35febeffd71ed4c9a1
julietbravo <bart@vanstratum.com>
2018-10-31 11:36:17 +0100

    Removed boundary nudging test case

--- cases/nudge_boundary/clean.py
+++
@@ -1,9 +0,0 @@
-import glob
-import os
-
-all_files = glob.glob('*')
-exclude   = ['namoptions.001','prof.inp.001','lscale.inp.001','run.PBS']
-
-for f in all_files:
-    if f not in exclude and '.py' not in f:
-        os.remove(f)
