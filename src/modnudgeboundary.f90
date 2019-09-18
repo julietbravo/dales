2e0c6551f5e829202b4076337affd62bd24f0052
julietbravo <bart@vanstratum.com>
2019-09-18 11:08:34 +0200

    Cleanup nudgeboundary + added offset for the perturbations

--- src/modnudgeboundary.f90
+++ src/modnudgeboundary.f90
@@ -27,15 +27,15 @@ implicit none
 
 public  :: initnudgeboundary, nudgeboundary, exitnudgeboundary
 save
-    logical :: lnudge_boundary = .false., lnudge_mean=.false., lperturb_boundary = .false., lsorbjan=.false.
+    logical :: lnudge_boundary = .false., lnudge_mean=.false., lperturb_boundary = .false.
     integer :: nudge_mode = 2  ! 1=to initial profile, 2=to mean profile
-    real, dimension(:), allocatable :: nudgefac_west,  nudgefac_east
-    real, dimension(:), allocatable :: nudgefac_south, nudgefac_north
+    real, dimension(:), allocatable :: nudgefac_west, perturbfac_west
     real, dimension(:), allocatable :: unudge, vnudge, thlnudge, qtnudge
     real, dimension(:), allocatable :: ub_westl, vb_westl, thlb_westl, qtb_westl
     real, dimension(:), allocatable :: ub_westg, vb_westg, thlb_westg, qtb_westg
     real, dimension(:), allocatable :: ut_west, vt_west, thlt_west, qtt_west
-    real :: nudge_offset=-1, nudge_width=-1, tau=-1, perturb_ampl=0, zmax_perturb=0
+    real :: nudge_offset=-1, nudge_width=-1, tau=-1
+    real :: perturb_offset=-1, perturb_width=-1, perturb_ampl=0, zmax_perturb=0
     integer :: blocksize=1, kmax_perturb=0
 
 contains
@@ -51,7 +51,8 @@ contains
         ! Read namelist settings
         !
         namelist /NAMNUDGEBOUNDARY/ lnudge_boundary, nudge_offset, nudge_width, tau, nudge_mode, &
-            & lperturb_boundary, perturb_ampl, blocksize, zmax_perturb, lsorbjan, lnudge_mean
+            & lperturb_boundary, perturb_offset, perturb_width, perturb_ampl, blocksize, zmax_perturb, &
+            & lnudge_mean
 
         if (myid==0) then
             open(ifnamopt, file=fname_options, status='old', iostat=ierr)
@@ -65,13 +66,14 @@ contains
 
         call MPI_BCAST(lnudge_boundary,   1, mpi_logical, 0, comm3d, mpierr)
         call MPI_BCAST(lperturb_boundary, 1, mpi_logical, 0, comm3d, mpierr)
-        call MPI_BCAST(lsorbjan,          1, mpi_logical, 0, comm3d, mpierr)
         call MPI_BCAST(lnudge_mean,       1, mpi_logical, 0, comm3d, mpierr)
         call MPI_BCAST(nudge_mode,        1, mpi_int,     0, comm3d, mpierr)
         call MPI_BCAST(blocksize,         1, mpi_int,     0, comm3d, mpierr)
         call MPI_BCAST(nudge_offset,      1, my_real,     0, comm3d, mpierr)
         call MPI_BCAST(nudge_width,       1, my_real,     0, comm3d, mpierr)
         call MPI_BCAST(tau,               1, my_real,     0, comm3d, mpierr)
+        call MPI_BCAST(perturb_offset,    1, my_real,     0, comm3d, mpierr)
+        call MPI_BCAST(perturb_width,     1, my_real,     0, comm3d, mpierr)
         call MPI_BCAST(perturb_ampl,      1, my_real,     0, comm3d, mpierr)
         call MPI_BCAST(zmax_perturb,      1, my_real,     0, comm3d, mpierr)
 
@@ -79,8 +81,7 @@ contains
             !
             ! Init and calculate nudge factors
             !
-            allocate( nudgefac_west(2:i1),  nudgefac_east(2:i1) )
-            allocate( nudgefac_south(2:j1), nudgefac_north(2:j1) )
+            allocate( nudgefac_west(2:i1),  perturbfac_west(2:i1) )
             allocate( unudge(k1), vnudge(k1), thlnudge(k1), qtnudge(k1) )
             allocate( ub_westl(k1), vb_westl(k1), thlb_westl(k1), qtb_westl(k1) )
             allocate( ub_westg(k1), vb_westg(k1), thlb_westg(k1), qtb_westg(k1) )
@@ -88,17 +89,18 @@ contains
 
             do i=2,i1
                 x = myidx * (xsize / nprocx) + (i-1.5)*dx
-                nudgefac_west(i) = exp(-0.5*((x-       nudge_offset )/nudge_width)**2)
-                nudgefac_east(i) = exp(-0.5*((x-(xsize-nudge_offset))/nudge_width)**2)
-            end do
-
-            do j=2,j1
-                y = myidy * (ysize / nprocy) + (j-1.5)*dy
-                nudgefac_south(j) = exp(-0.5*((y-       nudge_offset )/nudge_width)**2)
-                nudgefac_north(j) = exp(-0.5*((y-(ysize-nudge_offset))/nudge_width)**2)
+                nudgefac_west(i)   = exp(-0.5*((x-nudge_offset )/nudge_width)**2)
             end do
 
             if (lperturb_boundary) then
+                !
+                ! Calculate perturbation factor
+                !
+                do i=2,i1
+                    x = myidx * (xsize / nprocx) + (i-1.5)*dx
+                    perturbfac_west(i) = exp(-0.5*((x-perturb_offset )/perturb_width)**2)
+                end do
+
                 !
                 ! Find maximum grid level to which the perturbations are applied
                 !
@@ -224,20 +226,11 @@ contains
                 do k=1,kmax
                     do j=2,j1
                         do i=2,i1
-                            up(i,j,k)   = up(i,j,k)   + nudgefac_west(i) * tau_i * (unudge(k)   - (u0(i,j,k)+cu)) + &
-                                                    & + nudgefac_east(i) * tau_i * (unudge(k)   - (u0(i,j,k)+cu))
-
-                            vp(i,j,k)   = vp(i,j,k)   + nudgefac_west(i) * tau_i * (vnudge(k)   - (v0(i,j,k)+cv)) + &
-                                                    & + nudgefac_east(i) * tau_i * (vnudge(k)   - (v0(i,j,k)+cv))
-
-                            wp(i,j,k)   = wp(i,j,k)   + nudgefac_west(i) * tau_i * (0.          - w0(i,j,k)) + &
-                                                    & + nudgefac_east(i) * tau_i * (0.          - w0(i,j,k))
-
-                            thlp(i,j,k) = thlp(i,j,k) + nudgefac_west(i) * tau_i * (thlnudge(k) - thl0(i,j,k)) + &
-                                                    & + nudgefac_east(i) * tau_i * (thlnudge(k) - thl0(i,j,k))
-
-                            qtp(i,j,k)  = qtp(i,j,k)  + nudgefac_west(i) * tau_i * (qtnudge(k)  - qt0(i,j,k) ) + &
-                                                    & + nudgefac_east(i) * tau_i * (qtnudge(k)  - qt0(i,j,k) )
+                            up(i,j,k)   = up(i,j,k)   + nudgefac_west(i) * tau_i * (unudge(k)   - (u0(i,j,k)+cu))
+                            vp(i,j,k)   = vp(i,j,k)   + nudgefac_west(i) * tau_i * (vnudge(k)   - (v0(i,j,k)+cv))
+                            wp(i,j,k)   = wp(i,j,k)   + nudgefac_west(i) * tau_i * (0.          - w0(i,j,k))
+                            thlp(i,j,k) = thlp(i,j,k) + nudgefac_west(i) * tau_i * (thlnudge(k) - thl0(i,j,k))
+                            qtp(i,j,k)  = qtp(i,j,k)  + nudgefac_west(i) * tau_i * (qtnudge(k)  - qt0(i,j,k) )
                         end do
                     end do
                 end do
@@ -248,15 +241,6 @@ contains
             if (lperturb_boundary) then
 
                 do k=1,kmax_perturb
-                    if (lsorbjan) then
-                        ! BvS; even quicker-and-dirtier test using variance scaling from Sorbjan (1989)
-                        thetastr = -8e-3 / 0.28        ! BOMEX
-                        zi       = zmax_perturb
-
-                        perturb_ampl = 2*((2*(zf(k)/zi)**(-2/3.) * (1-(zf(k)/zi))**(4/3.) + 0.94*(zf(k)/zi)**(4/3.) * &
-                            & (1-(zf(k)/zi))**(-2/3.)) * thetastr**2.)**0.5
-                    end if
-
                     do blockj=0, jmax/blocksize-1
                         do blocki=0, imax/blocksize-1
                             perturbation = perturb_ampl*(rand(0)-0.5)
@@ -266,7 +250,10 @@ contains
                                     i = blocki*blocksize + subi + 2
                                     j = blockj*blocksize + subj + 2
 
-                                    thlp(i,j,k) = thlp(i,j,k) + nudgefac_west(i) * perturbation / rdt
+                                    if (perturbfac_west(i) > 0.01) then
+                                        !thlp(i,j,k) = thlp(i,j,k) + perturbfac_west(i) * perturbation / rdt
+                                        thlp(i,j,k) = thlp(i,j,k) + perturbation / rdt
+                                    end if
                                 end do
                             end do
 
@@ -281,7 +268,7 @@ contains
     subroutine exitnudgeboundary
         implicit none
         if (lnudge_boundary) then
-            deallocate( nudgefac_west, nudgefac_east, nudgefac_south, nudgefac_north )
+            deallocate( nudgefac_west, perturbfac_west )
             deallocate( unudge, vnudge, thlnudge, qtnudge)
         end if
     end subroutine exitnudgeboundary
